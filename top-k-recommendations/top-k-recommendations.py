a7b159b1d003c78cd8584783d3b766c7c08889c2
Manish <manishnalumachu@gmail.com>
2026-03-17 08:57:40 +0530

    Add solution for top-k-recommendations

---
+++ top-k-recommendations/top-k-recommendations.py
@@ -0,0 +1,5 @@
+def top_k_recommendations(scores, rated_indices, k):
+    candidates = [(scores[i], i) for i in range(len(scores)) if i not in rated_indices]
+    candidates.sort(key=lambda x: -x[0])
+    top_k = [idx for (_, idx) in candidates[:k]]    
+    return top_k
